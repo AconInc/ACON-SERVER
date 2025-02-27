140749283a16614ecb90b033a72ac4c0958b8613
ckkim817 <ckkim817@konkuk.ac.kr>
2025-02-28 03:56:55 +0900

    feat: feign 요청/응답 로깅 추가 (#96)

--- src/main/java/com/acon/server/global/config/OpenFeignConfig.java
+++ src/main/java/com/acon/server/global/config/OpenFeignConfig.java
@@ -1,6 +1,7 @@
 package com.acon.server.global.config;
 
 import com.acon.server.ServerApplication;
+import feign.Logger;
 import feign.Retryer;
 import feign.Retryer.Default;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -15,5 +16,11 @@ public class OpenFeignConfig {
     public Retryer retryer() {
         return new Default(1000, 1500, 2);
     }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
     // TODO: 타임아웃 설정 추가, 서킷 브레이커 적용하기
 }
