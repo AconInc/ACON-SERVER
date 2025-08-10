6d8d98e4307ee01dc82a15d8c60ad6db4bdc60ed
ckkim817 <ckkim817@konkuk.ac.kr>
2025-08-10 23:31:35 +0900

    hotfix: JwtAuthExp 발생 시 EntryPoint에서 JSON 바디까지 사용하도록 수정

--- src/main/java/com/acon/server/global/auth/filter/CustomJwtAuthenticationEntryPoint.java
+++ src/main/java/com/acon/server/global/auth/filter/CustomJwtAuthenticationEntryPoint.java
@@ -1,8 +1,15 @@
 package com.acon.server.global.auth.filter;
 
+import com.acon.server.global.auth.jwt.JwtAuthenticationException;
+import com.acon.server.global.dto.ErrorResponse;
+import com.acon.server.global.exception.ErrorType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,16 +19,26 @@ import org.springframework.stereotype.Component;
 public class CustomJwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     // 사용자가 인증되지 않은 상태에서 보호된 리소스에 접근하려고 할 때 호출
 
+    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(
             HttpServletRequest request,
             HttpServletResponse response,
             AuthenticationException authException
-    ) {
-        setResponse(response);
-    }
+    ) throws IOException {
+        if (authException instanceof JwtAuthenticationException jwtEx) {
+            ErrorType errorType = jwtEx.getErrorType();
 
-    private void setResponse(HttpServletResponse response) {
+            response.setStatus(errorType.getHttpStatus().value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+            objectMapper.writeValue(response.getOutputStream(), ErrorResponse.fail(errorType));
+
+            return;
+        }
+
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
