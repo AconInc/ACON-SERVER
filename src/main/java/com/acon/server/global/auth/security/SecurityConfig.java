4013d6cf370ce09d00b9407da96e94aba2723763
ckkim817 <ckkim817@konkuk.ac.kr>
2025-09-07 20:03:25 +0900

    hotfix: 환경별 쿠키 정책 하나로 통일

--- src/main/java/com/acon/server/global/auth/security/SecurityConfig.java
+++ src/main/java/com/acon/server/global/auth/security/SecurityConfig.java
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -65,21 +64,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    @Profile({"local", "dev"})
-    public CookieCsrfTokenRepository adminCsrfTokenRepositoryDev() {
-        var repo = CookieCsrfTokenRepository.withHttpOnlyFalse();
-
-        repo.setCookieCustomizer(cookie -> cookie
-                .sameSite("Lax")
-                .secure(false)
-                .path("/admin"));
-
-        return repo;
-    }
-
-    @Bean
-    @Profile("prod")
-    public CookieCsrfTokenRepository adminCsrfTokenRepositoryProd() {
+    public CookieCsrfTokenRepository adminCsrfTokenRepository() {
         var repo = CookieCsrfTokenRepository.withHttpOnlyFalse();
 
         repo.setCookieCustomizer(cookie -> cookie
