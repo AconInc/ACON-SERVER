1d48180d0d6ae7a81ec7d9794ec2407d5b4af66d
ckkim817 <ckkim817@konkuk.ac.kr>
2025-09-22 22:06:08 +0900

    hotfix: 영속성 엔티티 수정사항을 도메인 엔티티에도 반영

--- src/main/java/com/acon/server/spot/domain/entity/OpeningHour.java
+++ src/main/java/com/acon/server/spot/domain/entity/OpeningHour.java
@@ -11,21 +11,30 @@ public class OpeningHour {
     private final Long id;
     private final Long spotId;
     private final DayOfWeek dayOfWeek;
+    private final Boolean closed;
     private final LocalTime startTime;
     private final LocalTime endTime;
+    private final LocalTime breakStartTime;
+    private final LocalTime breakEndTime;
 
     @Builder
     public OpeningHour(
             final Long id,
             final Long spotId,
             final DayOfWeek dayOfWeek,
+            final Boolean closed,
             final LocalTime startTime,
-            final LocalTime endTime
+            final LocalTime endTime,
+            final LocalTime breakStartTime,
+            final LocalTime breakEndTime
     ) {
         this.id = id;
         this.spotId = spotId;
         this.dayOfWeek = dayOfWeek;
+        this.closed = closed;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.breakStartTime = breakStartTime;
+        this.breakEndTime = breakEndTime;
     }
 }
